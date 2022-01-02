e044b802a2fc6512fd2caa484d00563825efd811
Danil0v3s <daniloleemes@hotmail.com>
2022-01-02 14:17:19 +0000

    Add spotless to project

--- src/main/kotlin/ui/SideMenu.kt
+++ src/main/kotlin/ui/SideMenu.kt
@@ -1,17 +1,13 @@
 package ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 typealias Content = @Composable () -> Unit
